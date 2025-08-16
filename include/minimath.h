fbe79b1a79af10a8a37362e64d83f4504751c92c
legoisland21 <wrzochalmikolaj@gmail.com>
2025-08-16 11:28:27 +0200

    Add files via upload

--- include/minimath.h
+++ include/minimath.h
@@ -2,7 +2,6 @@
 #define MINIMATH_H
 #include <iostream>
 #include <vector>
-#include <math.h>
 #include "register.h"
 using namespace std;
 
@@ -14,7 +13,13 @@ void mulReg(int index, int index2, int index3) { registers[index3].ivalue = regi
 void divReg(int index, int index2, int index3) { if (registers[index2].ivalue != 0) registers[index3].ivalue = registers[index].ivalue / registers[index2].ivalue;
 else cout << "Error: divide by zero on R" << index2 << endl; }
 void modReg(int index, int index2, int index3) { registers[index3].ivalue = registers[index].ivalue % registers[index2].ivalue; }
-void powReg(int index, int index2, int index3) { registers[index3].ivalue = registers[index].ivalue ^ registers[index2].ivalue; }
+void powReg(int index, int index2, int index3) {
+    int base = registers[index].ivalue;
+    int exp = registers[index2].ivalue;
+    int result = 1;
+    for(int i = 0; i < exp; i++) result *= base;
+    registers[index3].ivalue = result;
+}
 void incReg(int index) { registers[index].ivalue++; }
 void decReg(int index) { registers[index].ivalue--; }
 #endif
