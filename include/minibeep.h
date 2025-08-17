97b544a94226c0342f014853b39df21ec3094ecd
legoisland21 <wrzochalmikolaj@gmail.com>
2025-08-17 10:54:59 +0200

    Add files via upload

--- include/minibeep.h
+++ include/minibeep.h
@@ -1,9 +1,10 @@
 #ifndef MINIBEEP_H
 #define MINIBEEP_H
+
 #include <thread>
 #include <chrono>
 #include <cmath>
-#include <iostream>
+#include <cstdio>
 
 using namespace std;
 
@@ -13,13 +14,18 @@ inline void beep(double freq, int ms) {
     Beep((DWORD)freq, (DWORD)ms);
 }
 #else
-#include <unistd.h>
 inline void beep(double freq, int ms) {
-    for(int i = 0; i < ms; i++) {
-        putchar(7);
-        fflush(stdout);
-        this_thread::sleep_for(chrono::milliseconds(1));
+    const int sampleRate = 44100;
+    const int samples = sampleRate * ms / 1000;
+
+    FILE* pipe = popen("aplay -q -f S16_LE -r 44100 -t raw > /dev/null 2>&1", "w");
+    if(!pipe) return;
+
+    for(int i = 0; i < samples; i++) {
+        short sample = 32767 * sin(2 * M_PI * freq * i / sampleRate);
+        fwrite(&sample, sizeof(short), 1, pipe);
     }
+    pclose(pipe);
 }
 #endif
 
