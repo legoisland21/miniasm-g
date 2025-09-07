7911244240022a66e03916341ec7f98de77c34dc
legoisland21 <wrzochalmikolaj@gmail.com>
2025-09-07 16:55:04 +0200

    added ANSI codes (space, tab etc..)

--- include/miniinput.h
+++ include/miniinput.h
@@ -1,3 +1,41 @@
+#ifndef MINIINPUT_H
+#define MINIINPUT_H
+
+#define I_NULL 0
+#define I_START_OF_HEADING 1
+#define I_START_OF_TEXT 2
+#define I_END_OF_TEXT 3
+#define I_END_OF_TRANSMISSION 4
+#define I_ENQUIRY 5
+#define I_ACKNOWLEDGE 6
+#define I_BELL 7
+#define I_BACKSPACE 8
+#define I_HORIZONTAL_TAB 9
+#define I_LINE_FEED 10
+#define I_VERTICAL_TAB 11
+#define I_FORM_FEED 12
+#define I_CARRIAGE_RETURN 13
+#define I_SHIFT_OUT 14
+#define I_SHIFT_IN 15
+#define I_DATA_LINK_ESCAPE 16
+#define I_DEVICE_CTRL_1 17
+#define I_DEVICE_CTRL_2 18
+#define I_DEVICE_CTRL_3 19
+#define I_DEVICE_CTRL_4 20
+#define I_NEGATIVE_ACK 21
+#define I_SYNCH_IDLE 22
+#define I_END_OF_BLOCK 23
+#define I_CANCEL 24
+#define I_END_OF_MEDIUM 25
+#define I_SUBSTITUTE 26
+#define I_ESCAPE 27
+#define I_FILE_SEPARATOR 28
+#define I_GROUP_SEPARATOR 29
+#define I_RECORD_SEPARATOR 30
+#define I_UNIT_SEPARATOR 31
+#define I_SPACE 32
+#define I_DELETE 127
+
 #ifdef _WIN32
 #include <conio.h>
 
@@ -46,4 +84,6 @@ int getInputNs() {
 
     return ch;
 }
+#endif
+
 #endif
