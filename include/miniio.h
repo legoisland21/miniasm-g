8f5457f34de3fde4d8acaadfb2236e7923b0ee76
legoisland21 <wrzochalmikolaj@gmail.com>
2025-09-08 18:52:24 +0200

    added newLine and fixed getInputStr to use getline

--- include/miniio.h
+++ include/miniio.h
@@ -11,8 +11,10 @@ void printInt(int index, bool newline) { cout << registers[index].ivalue; if(new
 void printStr(int index, bool newline) { cout << registers[index].svalue; if(newline) cout << endl; }
 
 void getInputInt(int index) { int result; cin >> result; registers[index].ivalue = result; }
-void getInputStr(int index) { string result; cin >> result; registers[index].svalue = result; }
+void getInputStr(int index) { string result; getline(cin, result); registers[index].svalue = result; }
 
 int readRegInt(int index) { return registers[index].ivalue; }
 string readRegStr(int index) { return registers[index].svalue; }
+
+void newLine() { cout << endl; }
 #endif
