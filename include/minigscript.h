ebfd63c8f5f52d94fd79d8193a74553555930fff
legoisland21 <wrzochalmikolaj@gmail.com>
2025-09-13 14:05:24 +0200

    G-Script support (GSC)

---
+++ include/minigscript.h
@@ -0,0 +1,52 @@
+#ifndef MINIGSCRIPT_H
+#define MINIGSCRIPT_H
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "minigraphics.h"
+
+using namespace std;
+
+inline void loadGScript(AsciiTable &screen, const string &path) {
+    ifstream file(path);
+    if (!file.is_open()) {
+        cerr << "Failed to open script: " << path << endl;
+        return;
+    }
+
+    int minW, minH;
+    if (!(file >> minW >> minH)) {
+        cerr << "Invalid header in script: " << path << endl;
+        return;
+    }
+
+    if (screen.getWidth() < minW || screen.getHeight() < minH) {
+        cerr << "Screen too small for script: "
+             << "requires at least " << minW << "x" << minH
+             << ", but screen is " << screen.getWidth() << "x" << screen.getHeight()
+             << endl;
+        return;
+    }
+
+    string line;
+    int y = 0;
+    getline(file, line);
+
+    while (getline(file, line)) {
+        if (line.empty()) continue;
+
+        stringstream ss(line);
+        int val, x = 0;
+        while (ss >> val) {
+            char ch = '#';
+            int fg = val;
+            int bg = BLACK;
+            screen.setPixel(x, y, ch, fg, fg);
+            x++;
+        }
+        y++;
+    }
+}
+
+#endif
